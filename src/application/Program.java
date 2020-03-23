729a32abcef9675183e6a0839136025e031314c4
Joao Pedro <vyrkshowe@gmail.com>
2020-03-22 23:56:10 -0300

    Desenvolvimento da classe departamento

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entites.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
+
+	}
+
+}
