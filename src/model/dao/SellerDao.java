37cfdfbf910d1811a70f815c63eea47fe9f9df82
Joao Pedro <vyrkshowe@gmail.com>
2020-03-23 00:46:06 -0300

    Desenvolvimento das interfaces de acesso

---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entites.Seller;
+
+public interface SellerDao {
+
+	
+	void insert(Seller obj);
+	void update(Seller obj);
+	void deleteById(Integer id);
+	Seller findById(Integer id);
+	List<Seller> findAll();
+}
